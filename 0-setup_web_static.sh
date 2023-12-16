f6ffe3fe8f99315f001b1c8061c102573bbfffb6
MthokozisiZwane <mthokozisi.zwane1999@gmail.com>
2023-12-16 08:18:01 -0800

    prepare webserers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,30 +1,35 @@
 #!/usr/bin/env bash
-# The script sets up my web servers for deployment
+# The script sets up my web servers for deployment of web_static.
 
-# Install Nginx if not already installed
-if ! command -v nginx &> /dev/null; then
-    sudo apt-get update
-    sudo apt-get -y install nginx
-fi
+apt-get update
+apt-get install -y nginx
 
-# Creates the  required directories
-sudo mkdir -p /data/web_static/releases/test
-sudo mkdir -p /data/web_static/shared
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "Holberton School" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-# Creates a fake HTML file
-echo "<html><head></head><body>Holberton School</body></html>" | sudo tee /data/web_static/releases/test/index.html
+chown -R ubuntu /data/
+chgrp -R ubuntu /data/
 
-# Creates or recreates the symbolic link
-sudo rm -f /data/web_static/current
-sudo ln -s /data/web_static/releases/test /data/web_static/current
+printf %s "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+    location /redirect_me {
+        return 301 http://live.fundza.mobi/writer-profiles/meet-the-fanz/mthokozisi-zwane/;
+    }
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}" > /etc/nginx/sites-available/default
 
-# Gives ownership recursively to ubuntu
-sudo chown -R ubuntu:ubuntu /data
-
-# Updates Nginx configuration
-NEW_STRING="\\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n"
-if ! grep -q "location /hbnb_static/" /etc/nginx/sites-available/default; then
-    sudo sed -i "/server {/a $NEW_STRING" /etc/nginx/sites-available/default
-fi
-# Restarst Nginx after changes
-sudo service nginx restart
+service nginx restart
